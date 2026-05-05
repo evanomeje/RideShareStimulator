03fd78c87d0d4b3486b0d3f7845175bab342f15d
evan <evanomeje@gmail.com>
2026-05-05 16:49:32 -0600

    testing deployment #2

--- http-server/main.go
+++ http-server/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func getData(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "Hello world\n")
+	fmt.Fprintf(w, "Hello world\n")
 }
 
 func main() {
-    http.HandleFunc("/data", getData)
-    http.ListenAndServe(":8080", nil)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/data", getData)
+	serverEnv := os.Getenv("SERVER_ENV")
+	if serverEnv == "DEV" {
+		http.ListenAndServe(":8080", nil)
+	} else if serverEnv == "PROD" {
+		http.ListenAndServeTLS(
+			":443",
+			"/etc/letsencrypt/live/app.evanomeje.xyz/fullchain.pem",
+			"/etc/letsencrypt/live/app.evanomeje.xyz/privkey.pem",
+			nil,
+		)
+	}
 }
