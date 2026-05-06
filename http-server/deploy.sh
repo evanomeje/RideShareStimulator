dd2408e0e4cfdf191dc274676e582e08d07cdb0c
evan <evanomeje@gmail.com>
2026-05-05 23:06:27 -0600

    migrate to react

--- http-server/deploy.sh
+++ http-server/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+cd frontend
+npm run build
+cd ..
+git add .
+git commit -m "build"
+git push origin main
 sshcmd="ssh -t evan@api.evanomeje.xyz"
 $sshcmd screen -S "deployment" /home/evan/RideShareStimulator/http-server/prod_deploy.sh
