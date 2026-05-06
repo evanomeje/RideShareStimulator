12ae86f835c607cd298dfb98e22c91c6825ff87f
evan <evanomeje@gmail.com>
2026-05-05 20:31:42 -0600

    switch to app subdomain

--- http-server/main.go
+++ http-server/main.go
@@ -13,14 +13,16 @@ func getData(w http.ResponseWriter, req *http.Request) {
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/data", getData)
+
 	serverEnv := os.Getenv("SERVER_ENV")
+
 	if serverEnv == "DEV" {
 		http.ListenAndServe(":8080", nil)
 	} else if serverEnv == "PROD" {
 		http.ListenAndServeTLS(
 			":443",
-			"/etc/letsencrypt/live/api.evanomeje.xyz/fullchain.pem",
-			"/etc/letsencrypt/live/api.evanomeje.xyz/privkey.pem",
+			"/etc/letsencrypt/live/app.evanomeje.xyz/fullchain.pem",
+			"/etc/letsencrypt/live/app.evanomeje.xyz/privkey.pem",
 			nil,
 		)
 	}
